02fb24d821f4af1c29b088cc4d7f9cabcf2f5616
melvinmcrn <greatwinblowaway@gmail.com>
2024-09-17 15:22:24 +0700

    minor: add name to aave-v3 tsup

--- packages/modules/aave-v3/tsup.config.ts
+++ packages/modules/aave-v3/tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
+  name: 'aave-v3',
   entry: ['src/index.ts', 'src/actions/index.ts', 'src/utils/index.ts'], // Entry points
   format: ['esm', 'cjs'], // Build for ESmodules and Commonjs
   dts: true, // Generate declaration file (.d.ts)
