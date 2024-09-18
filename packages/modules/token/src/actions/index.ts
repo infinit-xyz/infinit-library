6ae7dd8094889ebf92c4d26dd1bd7ea76b31603b
kuhaku <75209253+tapakornl@users.noreply.github.com>
2024-09-18 15:25:32 +0700

    fix(token): add setMerkleRoot into `actions` (#13)

--- packages/modules/token/src/actions/index.ts
+++ packages/modules/token/src/actions/index.ts
@@ -3,6 +3,7 @@ import { InfinitActionRecord } from '@infinit-xyz/core'
 import { DeployInfinitERC20Action, DeployInfinitERC20ActionParamsSchema } from '@actions/deployInfinitERC20'
 import { DeployInfinitERC20BurnableAction, DeployInfinitERC20BurnableActionParamsSchema } from '@actions/deployInfinitERC20Burnable'
 import { GetProofMerkleTreeAction, GetProofMerkleTreeActionParamsSchema } from '@actions/getProofMerkleTree'
+import { SetMerkleRootAction, SetMerkleRootActionParamsSchema } from '@actions/setMerkleRoot'
 
 import {
   DeployAccumulativeMerkleDistributorAction,
@@ -34,6 +35,18 @@ export const actions = {
     paramSchema: DeployAccumulativeMerkleDistributorProxyActionParamsSchema,
     signers: ['deployer'],
   },
+  setMerkleRootAction: {
+    name: 'Set Merkle Root',
+    actionClassName: SetMerkleRootAction.name,
+    paramSchema: SetMerkleRootActionParamsSchema,
+    signers: ['owner'],
+  },
 } satisfies InfinitActionRecord
 
-export { DeployAccumulativeMerkleDistributorAction, DeployInfinitERC20Action, DeployInfinitERC20BurnableAction, GetProofMerkleTreeAction }
+export {
+  DeployAccumulativeMerkleDistributorAction,
+  DeployInfinitERC20Action,
+  DeployInfinitERC20BurnableAction,
+  GetProofMerkleTreeAction,
+  SetMerkleRootAction,
+}
