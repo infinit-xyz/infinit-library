6dd4c442c9eaeef8138240cffdff106914c8d604
melvin 🦊 <42515663+melvinmcrn@users.noreply.github.com>
2024-09-23 15:47:15 +0700

    fix(uniswap-v3): remove unused mock contracts (#22)

--- packages/modules/uniswap-v3/contracts/openzeppelin-mock/TransparentUpgradeableProxyMock.sol
+++
@@ -1,8 +0,0 @@
-import "@openzeppelin/contracts/proxy/TransparentUpgradeableProxy.sol";
-
-// note: mock contract to be able to compile ProxyAdmin.sol from node_modules
-contract TrasnparentUpgradeableProxyMock is TransparentUpgradeableProxy {
-    constructor(address _logic, address _admin, bytes memory _data)
-        TransparentUpgradeableProxy(_logic, _admin, _data)
-    {}
-}
