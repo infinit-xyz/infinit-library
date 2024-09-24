6466b340580160dbc23b5e111b958a8b79517632
tapakornl <tapakorn.loet@gmail.com>
2024-09-24 14:27:56 +0700

    feat: aave v3 set emission admin action

---
+++ packages/modules/aave-v3/src/actions/subactions/setEmissionAdmin.ts
@@ -0,0 +1,29 @@
+import { Hash } from 'viem'
+
+import { InfinitWallet, SubAction, SubActionExecuteResponse } from '@infinit-xyz/core'
+
+import { SetEmissionAdminParams, SetEmissionAdminTxBuilder } from '@actions/subactions/tx-builders/emissionManager/setEmissionAdmin'
+
+import { AaveV3Registry } from '@/src/type'
+
+export type SetEmissionAdminSubActionParams = SetEmissionAdminParams
+
+export class SetEmissionAdminSubAction extends SubAction<SetEmissionAdminSubActionParams, Object, Object> {
+  constructor(client: InfinitWallet, params: SetEmissionAdminSubActionParams) {
+    super(SetEmissionAdminSubAction.name, client, params)
+  }
+
+  protected setTxBuilders(): void {
+    this.txBuilders.push(new SetEmissionAdminTxBuilder(this.client, this.params))
+  }
+
+  protected async updateRegistryAndMessage(
+    registry: AaveV3Registry,
+    _txHashes: Hash[],
+  ): Promise<SubActionExecuteResponse<AaveV3Registry, {}>> {
+    return {
+      newRegistry: registry,
+      newMessage: {},
+    }
+  }
+}
---
+++ packages/modules/aave-v3/src/actions/subactions/tx-builders/emissionManager/setEmissionAdmin.ts
@@ -0,0 +1,56 @@
+import { Address, encodeFunctionData, zeroAddress } from 'viem'
+
+import { InfinitWallet, TransactionData, TxBuilder } from '@infinit-xyz/core'
+import { ContractValidateError } from '@infinit-xyz/core/errors'
+
+import { readArtifact } from '@/src/utils/artifact'
+
+export type SetEmissionAdminParams = {
+  emissionManager: Address
+  reward: Address
+  admin: Address
+}
+
+export class SetEmissionAdminTxBuilder extends TxBuilder {
+  emissionManager: Address
+  reward: Address
+  admin: Address
+
+  constructor(client: InfinitWallet, params: SetEmissionAdminParams) {
+    super(SetEmissionAdminTxBuilder.name, client)
+    this.emissionManager = params.emissionManager
+    this.reward = params.reward
+    this.admin = params.admin
+  }
+
+  async buildTx(): Promise<TransactionData> {
+    const emissionManagerArtifact = await readArtifact('EmissionManager')
+
+    const callData = encodeFunctionData({
+      abi: emissionManagerArtifact.abi,
+      functionName: 'setEmissionAdmin',
+      args: [this.reward, this.admin],
+    })
+
+    const tx: TransactionData = {
+      data: callData,
+      to: this.emissionManager,
+    }
+    return tx
+  }
+
+  public async validate(): Promise<void> {
+    if (this.emissionManager === zeroAddress) throw new ContractValidateError('emissionManager cannot be zero address')
+    if (this.reward === zeroAddress) throw new ContractValidateError('reward cannot be zero address')
+
+    const poolAddressesProviderArtifact = await readArtifact('EmissionManager')
+    const owner = await this.client.publicClient.readContract({
+      address: this.emissionManager,
+      abi: poolAddressesProviderArtifact.abi,
+      functionName: 'owner',
+      args: [],
+    })
+
+    if (owner !== this.client.walletClient.account.address) throw new ContractValidateError('caller is not owner')
+  }
+}
