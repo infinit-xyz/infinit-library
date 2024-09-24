6986edbcaa447281674f2b33d7aafd89d1373945
kuhaku <75209253+tapakornl@users.noreply.github.com>
2024-09-24 17:29:31 +0700

    feat(aave-v3): set emission admin (#36)
    Co-authored-by: tapakornl <tapakornl@users.noreply.github.com>
    Co-authored-by: melvin 🦊 <42515663+melvinmcrn@users.noreply.github.com>
    Co-authored-by: melvinmcrn <greatwinblowaway@gmail.com>

---
+++ packages/modules/aave-v3/src/actions/__test__/setEmissionAdmin.test.ts
@@ -0,0 +1,68 @@
+import { beforeAll, describe, expect, test, vi } from 'vitest'
+
+import { Address } from 'viem'
+
+import { ARBITRUM_TEST_ADDRESSES, TEST_ADDRESSES } from '@actions/__mock__/address'
+import { setupAaveV3 } from '@actions/__mock__/setup'
+import { SetEmissionAdminAction } from '@actions/setEmissionAdmin'
+
+import { AaveV3Registry } from '@/src/type'
+import { TestChain, TestInfinitWallet } from '@infinit-xyz/test'
+import { readArtifact } from '@utils/artifact'
+
+vi.mock('@actions/subactions/setLtvSubAction')
+
+describe('EmissionManager:SetEmissionAdminAction', () => {
+  let action: SetEmissionAdminAction
+  let client: TestInfinitWallet
+  let bob: TestInfinitWallet
+  let registry: AaveV3Registry
+  let emissionManager: Address
+
+  beforeAll(async () => {
+    client = new TestInfinitWallet(TestChain.arbitrum, ARBITRUM_TEST_ADDRESSES.tester)
+    bob = new TestInfinitWallet(TestChain.arbitrum, TEST_ADDRESSES.bob)
+    registry = await setupAaveV3()
+
+    emissionManager = registry.emissionManager!
+  })
+
+  test('not admin shouldnt be able to call', async () => {
+    action = new SetEmissionAdminAction({
+      params: {
+        emissionManager: emissionManager,
+        reward: ARBITRUM_TEST_ADDRESSES.usdt,
+        admin: ARBITRUM_TEST_ADDRESSES.tester,
+      },
+      signer: {
+        emissionManagerOwner: bob,
+      },
+    })
+    await expect(action.run(registry)).rejects.toThrowError('caller is not owner')
+  })
+
+  test('should run successfully', async () => {
+    action = new SetEmissionAdminAction({
+      params: {
+        emissionManager: emissionManager,
+        reward: ARBITRUM_TEST_ADDRESSES.usdt,
+        admin: ARBITRUM_TEST_ADDRESSES.tester,
+      },
+      signer: {
+        emissionManagerOwner: client,
+      },
+    })
+    await action.run(registry)
+
+    // read abi
+    const emissionManagerArtifact = await readArtifact('EmissionManager')
+    // read owner
+    const emissionAdmin: Address = await client.publicClient.readContract({
+      address: emissionManager,
+      abi: emissionManagerArtifact.abi,
+      functionName: 'getEmissionAdmin',
+      args: [ARBITRUM_TEST_ADDRESSES.usdt],
+    })
+    expect(emissionAdmin).toBe(ARBITRUM_TEST_ADDRESSES.tester)
+  })
+})
---
+++ packages/modules/aave-v3/src/actions/setEmissionAdmin.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod'
+
+import { Action, InfinitWallet, SubAction } from '@infinit-xyz/core'
+import { validateActionData, zodAddressNonZero } from '@infinit-xyz/core/internal'
+
+import { SetEmissionAdminSubAction, SetEmissionAdminSubActionParams } from '@actions/subactions/setEmissionAdmin'
+
+export const SetEmissionAdminActionParamsSchema = z.object({
+  emissionManager: zodAddressNonZero.describe(`The address of the emission manager contract e.g. '0x123...abc'`),
+  reward: zodAddressNonZero.describe(`The address of the reward token e.g. '0x123...abc'`),
+  admin: zodAddressNonZero.describe(`The address of the reward admin specific to this reward token e.g. '0x123...abc'`),
+}) satisfies z.ZodType<SetEmissionAdminSubActionParams>
+
+export type SetEmissionAdminData = {
+  params: SetEmissionAdminSubActionParams
+  signer: Record<'emissionManagerOwner', InfinitWallet>
+}
+
+export class SetEmissionAdminAction extends Action<SetEmissionAdminData> {
+  constructor(data: SetEmissionAdminData) {
+    console.log('data', data)
+    validateActionData(data, SetEmissionAdminActionParamsSchema, ['emissionManagerOwner'])
+    super(SetEmissionAdminAction.name, data)
+  }
+  protected getSubActions(): SubAction[] {
+    const owner = this.data.signer['emissionManagerOwner']
+    const params = this.data.params
+
+    return [new SetEmissionAdminSubAction(owner, params)]
+  }
+}
