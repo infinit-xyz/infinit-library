87b4c48dfdee065bc41742100c07dac2ad27b23b
melvin 🦊 <42515663+melvinmcrn@users.noreply.github.com>
2024-10-02 23:31:59 +0700

    feat(core): add validation error (#57)

--- packages/core/src/errors/validate.test.ts
+++ packages/core/src/errors/validate.test.ts
@@ -1,38 +1,62 @@
 import { name as coreName, version as coreVersion } from 'package.json'
 import { describe, expect, test } from 'vitest'
 
-import { ValidateInputValueError, ValueNotFoundError } from '@errors/index'
+import { ValidateInputValueError, ValidateInputZeroAddressError, ValidateLengthError, ValueNotFoundError } from './validate'
 
-describe('ValidateInputValueError', () => {
-  test('should match snapshot', () => {
-    expect(new ValidateInputValueError()).toMatchInlineSnapshot(`
+describe('errors/validate.ts', () => {
+  describe('ValidateInputValueError', () => {
+    test('should match snapshot', () => {
+      expect(new ValidateInputValueError()).toMatchInlineSnapshot(`
         [ValidateInputValueError: Please check your input params
 
         ${coreName}: ${coreVersion}]
     `)
 
-    expect(new ValidateInputValueError('extra error')).toMatchInlineSnapshot(`
+      expect(new ValidateInputValueError('extra error')).toMatchInlineSnapshot(`
         [ValidateInputValueError: Please check your input params
         extra error
 
         ${coreName}: ${coreVersion}]
     `)
+    })
   })
-})
 
-describe('ValueNotFoundError', () => {
-  test('should match snapshot', () => {
-    expect(new ValueNotFoundError()).toMatchInlineSnapshot(`
+  describe('ValueNotFoundError', () => {
+    test('should match snapshot', () => {
+      expect(new ValueNotFoundError()).toMatchInlineSnapshot(`
         [ValueNotFoundError: Please check your input params
 
         ${coreName}: ${coreVersion}]
     `)
 
-    expect(new ValueNotFoundError('extra error')).toMatchInlineSnapshot(`
+      expect(new ValueNotFoundError('extra error')).toMatchInlineSnapshot(`
         [ValueNotFoundError: Please check your input params
         extra error
 
         ${coreName}: ${coreVersion}]
     `)
+    })
+  })
+
+  describe('ValueNotFoundError', () => {
+    test('should match snapshot', () => {
+      expect(new ValidateLengthError()).toMatchInlineSnapshot(`
+        [ValidateInputValueError: Please check your input params
+        LENGTH_MISMATCH
+
+        ${coreName}: ${coreVersion}]
+    `)
+    })
+  })
+
+  describe('ValueNotFoundError', () => {
+    test('should match snapshot', () => {
+      expect(new ValidateInputZeroAddressError('owner')).toMatchInlineSnapshot(`
+        [ValidateInputValueError: Please check your input params
+        owner SHOULD_NOT_BE_ZERO_ADDRESS
+
+        ${coreName}: ${coreVersion}]
+    `)
+    })
   })
 })
