62c821bd8df8ac6487cf3ce9c9b97c24b6fc528b
melvinmcrn <greatwinblowaway@gmail.com>
2024-10-02 22:47:47 +0700

    update export errors test

--- packages/core/src/exports/errors.test.ts
+++ packages/core/src/exports/errors.test.ts
@@ -3,18 +3,5 @@ import { expect, test } from 'vitest'
 import * as errors from './errors'
 
 test('exports - errors: should match snapshot', () => {
-  expect(Object.keys(errors)).toMatchInlineSnapshot(`
-    [
-      "FoundInvalidCachedTxError",
-      "IncorrectCacheError",
-      "ContractValidateError",
-      "DirectoryNotFoundError",
-      "InfinitWalletNotFoundError",
-      "TransactionError",
-      "ContractNotFoundError",
-      "TxNotFoundError",
-      "ValidateInputValueError",
-      "ValueNotFoundError",
-    ]
-  `)
+  expect(Object.keys(errors)).toMatchSnapshot()
 })
