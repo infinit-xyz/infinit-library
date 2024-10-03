c9a134c80e3b205ddd50859a2a168c08a95db5f5
melvinmcrn <greatwinblowaway@gmail.com>
2024-10-03 15:44:54 +0700

    update `ContractVerifier` constructor

--- packages/core/src/utils/verifier/ContractVerifier.ts
+++ packages/core/src/utils/verifier/ContractVerifier.ts
@@ -1,4 +1,4 @@
-import { Chain, PublicClient, createPublicClient, http } from 'viem'
+import { PublicClient } from 'viem'
 
 import { BlockExplorerParams, ContractVerifierCallback } from './type'
 import { Etherscan } from '@nomicfoundation/hardhat-verify/etherscan'
@@ -7,14 +7,11 @@ export abstract class ContractVerifier<R extends object> {
   protected client: PublicClient
   protected etherscan: Etherscan
 
-  constructor(chain: Chain, rpcEndpoint: string, params: BlockExplorerParams) {
+  constructor(client: PublicClient, params: BlockExplorerParams) {
     const parsedApiKey = params.apiKey ?? '' // TODO: test that we can use default value
 
     this.etherscan = new Etherscan(parsedApiKey, params.apiUrl, params.url)
-    this.client = createPublicClient({
-      chain,
-      transport: http(rpcEndpoint),
-    })
+    this.client = client
   }
 
   public abstract verify(registry: R, callback?: ContractVerifierCallback): Promise<void>
