17a9d97ecc9424cddba1046d12c9abe93543265c
melvinmcrn <greatwinblowaway@gmail.com>
2024-10-03 18:57:36 +0700

    update ts-doc

--- packages/core/src/utils/contract-verifier/type.ts
+++ packages/core/src/utils/contract-verifier/type.ts
@@ -1,31 +1,59 @@
 import { Address } from 'viem'
 
+/**
+ * Information about a contract.
+ */
 type ContractInfo = {
+  /**
+   * The address of the contract.
+   */
   address: Address
+  /**
+   * Optional constructor arguments for the contract.
+   */
   constructorArgs?: any[]
 }
 
+/**
+ * Parameters for the block explorer.
+ */
 type BlockExplorerParams = {
+  /**
+   * Optional API key for the block explorer.
+   */
   apiKey?: string
+  /**
+   * The API URL for the block explorer.
+   */
   apiUrl: string
+  /**
+   * The URL for the block explorer.
+   */
   url: string
 }
 
+/**
+ * Parameters for callback events.
+ */
 type CallbackParams = {
+  /**
+   * Information about the total number of contracts to verify.
+   */
   contractVerificationInfo: { totalContracts: number }
   /**
    * Event emitted when a contract verification starts.
-   * Contains the contract name and the contract address to verify
+   * Contains the contract name and the contract address to verify.
    */
   contractVerificationStarted: { contractName: string; address: Address }
   /**
-   * Event emitted when a contract verification submits
-   * Contains the the contract name and the contract address to verify and guid of verification
+   * Event emitted when a contract verification submits.
+   * Contains the contract name, the contract address to verify, and the GUID of the verification.
    */
   contractVerificationSubmitted: { contractName: string; address: Address; guid: string }
   /**
-   * Event emitted when a contract verification finished.
-   * Contain the contract name and the contract address to verify and url of that contract
+   * Event emitted when a contract verification finishes.
+   * Contains the contract name, the contract address to verify, and the URL of that contract.
+   * Also indicates if the contract was already verified.
    */
   contractVerificationFinished: { contractName: string; address: Address; url: string; isAlreadyVerified: boolean }
 }
@@ -35,6 +63,14 @@ type CallbackParams = {
  */
 type CallbackKeys = keyof CallbackParams
 
+/**
+ * Callback function for contract verification events.
+ *
+ * @template T - The type of the callback key.
+ * @param key - The key of the callback event.
+ * @param value - The value associated with the callback event.
+ * @returns A promise that resolves when the callback is complete.
+ */
 type ContractVerifierCallback = <T extends CallbackKeys>(key: T, value: CallbackParams[T]) => Promise<void>
 
 export { BlockExplorerParams, ContractInfo, ContractVerifierCallback }
