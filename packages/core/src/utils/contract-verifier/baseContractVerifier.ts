65552232e14591edbcec21cdc7f1f5494fde4276
kittikornl <pkorn003@gmail.com>
2024-10-07 14:50:02 +0700

    fix: change default value

--- packages/core/src/utils/contract-verifier/baseContractVerifier.ts
+++ packages/core/src/utils/contract-verifier/baseContractVerifier.ts
@@ -2,9 +2,9 @@ import { PublicClient } from 'viem'
 
 import { Artifacts } from 'hardhat/types'
 
+import { Etherscan } from '@nomicfoundation/hardhat-verify/etherscan.js'
 import { verifyContract } from './helper'
 import { BlockExplorerParams, ContractInfo, ContractVerifierCallback } from './type'
-import { Etherscan } from '@nomicfoundation/hardhat-verify/etherscan.js'
 
 /**
  * Abstract base class for contract verification.
@@ -22,7 +22,11 @@ export abstract class BaseContractVerifier<R extends object> {
    * @param params - The parameters for the block explorer.
    */
   constructor(client: PublicClient, params: BlockExplorerParams) {
-    const parsedApiKey = params.apiKey ?? '' // TODO: test that we can use default value
+    // NOTE: 
+    // 1. We can use Etherscan class for Blockscout API as well.
+    // 2. Blockscout API key is not required. but Etherscan client requires it.
+    // so we use a default value if it is not provided.
+    const parsedApiKey = params.apiKey ?? 'BLOCKSCOUT'
 
     this.etherscan = new Etherscan(parsedApiKey, params.apiUrl, params.url)
     this.client = client
