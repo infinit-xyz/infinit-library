1d321fb3baed7dd48c44dddff8a0bb92ca35c642
melvinmcrn <greatwinblowaway@gmail.com>
2024-10-09 22:08:54 +0700

    format and lint

--- packages/modules/token/src/utils/contract-verifier.test.ts
+++ packages/modules/token/src/utils/contract-verifier.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, test } from 'vitest'
 
 import { createPublicClient, http } from 'viem'
 import { holesky } from 'viem/chains'
+
 import { TokenRegistry, TokenType } from '../type'
 import { TokenContractVerifier } from './contract-verifier'
 
@@ -14,36 +15,35 @@ class TestTokenContractVerifier extends TokenContractVerifier {
 
 describe('TokenContractVerifier.ts', () => {
   const registry: TokenRegistry = {
-    "tokens": {
-      "0x86ce7353a19a6dbdb0a0ac01536ffc60f704980e": {
+    tokens: {
+      '0x86ce7353a19a6dbdb0a0ac01536ffc60f704980e': {
         type: TokenType.InfinitERC20,
       },
-      "0x445aa85126dd02145e194e31b19ebac0d051e4e2": {
+      '0x445aa85126dd02145e194e31b19ebac0d051e4e2': {
         type: TokenType.InfinitERC20Burnable,
-      }
-    },
-    "accumulativeMerkleDistributors": {
-      "0xab7819826b1acd87e30fa45e359e4038683365e6": {
-        "implementation": "0x78bb7fd5fc49a644f7b07e43812ef9535fbcfd29"
       },
-      "0x63743aaf4522ad8c47ba84b2fd2f645710b4454a": {
-        "implementation": "0x56313c36ba155bab1a5d569f48eaa965e270399d"
-      }
-    }
+    },
+    accumulativeMerkleDistributors: {
+      '0xab7819826b1acd87e30fa45e359e4038683365e6': {
+        implementation: '0x78bb7fd5fc49a644f7b07e43812ef9535fbcfd29',
+      },
+      '0x63743aaf4522ad8c47ba84b2fd2f645710b4454a': {
+        implementation: '0x56313c36ba155bab1a5d569f48eaa965e270399d',
+      },
+    },
   }
 
-  
   test('getContracts: should match snapshot', async () => {
     const verifier = new TestTokenContractVerifier(
       await createPublicClient({
         chain: holesky,
-        transport: http('https://1rpc.io/holesky')
+        transport: http('https://1rpc.io/holesky'),
       }),
       {
         apiKey: process.env.HOLESKY_ETHERSCAN_API_KEY ?? '',
         apiUrl: 'https://api-holesky.etherscan.io/api',
-        url: 'https://holesky.etherscan.io'
-      }
+        url: 'https://holesky.etherscan.io',
+      },
     )
     expect(verifier.testGetContracts(registry)).resolves.toMatchSnapshot()
   })
--- packages/modules/uniswap-v3/src/utils/contract-verifier.test.ts
+++ packages/modules/uniswap-v3/src/utils/contract-verifier.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, test } from 'vitest'
 
 import { createPublicClient, http } from 'viem'
 import { holesky } from 'viem/chains'
+
 import { UniswapV3Registry } from '../type'
 import { UniswapV3ContractVerifier } from './contract-verifier'
 
@@ -14,36 +15,33 @@ class TestUniswapV3ContractVerifier extends UniswapV3ContractVerifier {
 
 describe('UniswapV3ContractVerifier.ts', () => {
   const registry: UniswapV3Registry = {
-    "uniswapV3Factory": "0x8f0c29795615148f30bd402876fa69cbf88a91d4",
-    "nftDescriptor": "0xd66d72c3e44037229d01cd28ec541c10d8989ff1",
-    "tickLens": "0xac80bb20b9f763de59557bb0d88936800da7620e",
-    "proxyAdmin": "0x9cd6a1f83e35f40910ae5b59d86e3eb03ca32d21",
-    "quoterV2": "0x90a15ed68e1bd5eb02d972de6dde6fa72b0ff3c7",
-    "nonfungibleTokenPositionDescriptorImpl": "0x318cedc9cd4ead794eb3ff09de67463a0a476425",
-    "nonfungibleTokenPositionDescriptor": "0x2ffa05a4e3a06b1cb3716e294a31bd10356cd2ac",
-    "nonfungiblePositionManager": "0xc421820a10be395d41a25873f45e27b5cea80dfe",
-    "swapRouter02": "0xee6188037ab4f10a071cfa549fde21dc415e78a3",
-    "uniswapV3Staker": "0xbfc8692eded1c443569c17e7e6618033d533931c",
-    "permit2": "0xfd1f634f6c4b8f7b63ed9b32e748a049b28e743f",
-    "unsupportedProtocol": "0xc09eec68cd04842b213507ad0e0acbfcce87316a",
-    "universalRouter": "0x6ab3a902b67f0caf98c4714557845840e9645a5c",
-    "pools": [
-      "0x2135ea24a418c8312b302dfce8eb3897b4c68804"
-    ]
+    uniswapV3Factory: '0x8f0c29795615148f30bd402876fa69cbf88a91d4',
+    nftDescriptor: '0xd66d72c3e44037229d01cd28ec541c10d8989ff1',
+    tickLens: '0xac80bb20b9f763de59557bb0d88936800da7620e',
+    proxyAdmin: '0x9cd6a1f83e35f40910ae5b59d86e3eb03ca32d21',
+    quoterV2: '0x90a15ed68e1bd5eb02d972de6dde6fa72b0ff3c7',
+    nonfungibleTokenPositionDescriptorImpl: '0x318cedc9cd4ead794eb3ff09de67463a0a476425',
+    nonfungibleTokenPositionDescriptor: '0x2ffa05a4e3a06b1cb3716e294a31bd10356cd2ac',
+    nonfungiblePositionManager: '0xc421820a10be395d41a25873f45e27b5cea80dfe',
+    swapRouter02: '0xee6188037ab4f10a071cfa549fde21dc415e78a3',
+    uniswapV3Staker: '0xbfc8692eded1c443569c17e7e6618033d533931c',
+    permit2: '0xfd1f634f6c4b8f7b63ed9b32e748a049b28e743f',
+    unsupportedProtocol: '0xc09eec68cd04842b213507ad0e0acbfcce87316a',
+    universalRouter: '0x6ab3a902b67f0caf98c4714557845840e9645a5c',
+    pools: ['0x2135ea24a418c8312b302dfce8eb3897b4c68804'],
   }
 
-  
   test('getContracts: should match snapshot', async () => {
     const verifier = new TestUniswapV3ContractVerifier(
       await createPublicClient({
         chain: holesky,
-        transport: http('https://1rpc.io/holesky')
+        transport: http('https://1rpc.io/holesky'),
       }),
       {
         apiKey: process.env.HOLESKY_ETHERSCAN_API_KEY ?? '',
         apiUrl: 'https://api-holesky.etherscan.io/api',
-        url: 'https://holesky.etherscan.io'
-      }
+        url: 'https://holesky.etherscan.io',
+      },
     )
     expect(verifier.testGetContracts(registry)).resolves.toMatchSnapshot()
   })
