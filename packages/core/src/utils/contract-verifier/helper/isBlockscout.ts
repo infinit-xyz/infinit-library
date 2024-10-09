b59419756ca5ad611d11490e4668fd40cee49b22
melvinmcrn <greatwinblowaway@gmail.com>
2024-10-09 13:57:08 +0700

    refactor: isBlockscout

--- packages/core/src/utils/contract-verifier/helper/isBlockscout.ts
+++ packages/core/src/utils/contract-verifier/helper/isBlockscout.ts
@@ -1,22 +1,30 @@
 import axios from 'axios'
 
-export const isBlockscout = async (apiUrl: string) => {
+/**
+ * Checks if the given API URL is a Blockscout instance.
+ *
+ * @param apiUrl - The API URL to check.
+ * @returns A promise that resolves to a boolean indicating whether the API URL is a Blockscout instance.
+ * @throws Will throw an error if the API returns a status other than 200 or 404.
+ */
+export const isBlockscout = async (apiUrl: string): Promise<boolean> => {
   // note: blockscout has a /v2/stats endpoint
   const url = new URL(apiUrl + '/v2/stats')
-  let isBlockscout: boolean = true
-  await axios
+
+  return axios
     .get(url.toString())
-    .then((response: any) => {
+    .then((response) => {
       if (response.status === 200) {
-        isBlockscout = true
+        return true
       }
+
+      throw new Error(`Blockscout API V2 returned status ${response.status}`)
     })
     .catch((error: any) => {
       if (error.response.status === 404) {
-        isBlockscout = false
+        return false
       } else {
         throw new Error(error)
       }
     })
-  return isBlockscout
 }
