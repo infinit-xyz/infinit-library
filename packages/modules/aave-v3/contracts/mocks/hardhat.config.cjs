93035449348e70eff2a681e4f259434db9fedff2
melvin 🦊 <42515663+melvinmcrn@users.noreply.github.com>
2024-10-10 15:12:12 +0700

    Revert "refactor(aave-v3): update contracts to use symlink (#76)"
    This reverts commit 48f503845b41b0e3969dad3d3e05c3e045f525a2.

--- packages/modules/aave-v3/contracts/mocks/hardhat.config.cjs
+++ packages/modules/aave-v3/contracts/mocks/hardhat.config.cjs
@@ -1,8 +1,10 @@
 const { baseHardhatUserConfig } = require('@infinit-xyz/core/internal/hardhat-base-config')
 const { name } = require('../../package.json')
 
+const remappings = [`@aave/core-v3/contracts=aave-v3-core`]
+
 const config = {
-  ...baseHardhatUserConfig(__dirname, name),
+  ...baseHardhatUserConfig(__dirname, name, remappings),
   solidity: {
     compilers: [
       {
