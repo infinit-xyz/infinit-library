79003360c20b75e7da4ab15c75453b07927cfa5c
melvinmcrn <greatwinblowaway@gmail.com>
2024-10-24 15:21:44 +0700

    add registry

--- packages/core/src/base/offChainAction.ts
+++ packages/core/src/base/offChainAction.ts
@@ -1,4 +1,4 @@
-import { OffChainActionCallback } from '@/types'
+import { OffChainActionCallback } from '@/types/callback/offChainAction'
 
 /**
  * The return type for off-chain actions.
@@ -11,7 +11,7 @@ type OffChainActionReturn<D extends object | string | [] | undefined> = {
    */
   message?: string
   /**
-   * Optional data returned by the action.
+   * Data returned by the action.
    */
   data: D
 }
@@ -19,10 +19,15 @@ type OffChainActionReturn<D extends object | string | [] | undefined> = {
 /**
  * Abstract base class for off-chain actions.
  *
+ * @template R - The type of the registry object.
  * @template P - The type of the parameters required by the action.
  * @template D - The type of the data returned by the action.
  */
-abstract class OffChainAction<P extends object | undefined = undefined, D extends object | string | [] | undefined = undefined> {
+abstract class OffChainAction<
+  R extends object,
+  P extends object | undefined = undefined,
+  D extends object | string | [] | undefined = undefined,
+> {
   /**
    * Creates an instance of OffChainAction.
    */
@@ -30,16 +35,21 @@ abstract class OffChainAction<P extends object | undefined = undefined, D extend
     this.initialize()
   }
 
+  /**
+   * Abstract method to initialize the off-chain action.
+   * This method should be implemented by subclasses to perform any necessary setup.
+   */
   abstract initialize(): void
 
   /**
    * Abstract method to run the off-chain action.
    *
+   * @param registry - The registry object required to run the action.
    * @param params - The parameters required to run the action.
    * @param callback - Optional callback for action events.
    * @returns A promise that resolves to the result of the action.
    */
-  abstract run(params: P, callback?: OffChainActionCallback): Promise<OffChainActionReturn<D>>
+  abstract run(registry: R, params: P, callback?: OffChainActionCallback): Promise<OffChainActionReturn<D>>
 }
 
 export { OffChainAction, OffChainActionReturn }
