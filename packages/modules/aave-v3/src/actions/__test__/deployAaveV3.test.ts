1b464cc5c82dbd87c1301b0539d9252c216daaa3
melvinmcrn <greatwinblowaway@gmail.com>
2024-10-28 18:26:50 +0700

    typo

--- packages/modules/aave-v3/src/actions/__test__/deployAaveV3.test.ts
+++ packages/modules/aave-v3/src/actions/__test__/deployAaveV3.test.ts
@@ -210,236 +210,232 @@ describe('deployAaveV3Action', () => {
     expect(owner).toBe(oneAddress)
   })
 
-  test(
-    'simple run (different addreses on each roles) + init reserves',
-    async () => {
-      const deployer = client.walletClient.account.address
-      const weth9 = await readArtifact('WETH9')
-      const callData = encodeFunctionData({ abi: weth9.abi, functionName: 'deposit', args: [] })
-      const tx: TransactionData = {
-        data: callData,
-        to: weth,
-        value: BigInt(10 ** 18),
-      }
+  test('simple run (different addreses on each roles) + init reserves', { retry: 5 }, async () => {
+    const deployer = client.walletClient.account.address
+    const weth9 = await readArtifact('WETH9')
+    const callData = encodeFunctionData({ abi: weth9.abi, functionName: 'deposit', args: [] })
+    const tx: TransactionData = {
+      data: callData,
+      to: weth,
+      value: BigInt(10 ** 18),
+    }
 
-      await client.sendTransactions([{ name: 'Mint WETH', txData: tx }])
+    await client.sendTransactions([{ name: 'Mint WETH', txData: tx }])
 
-      action = new DeployAaveV3Action({
-        params: {
-          treasuryOwner: deployer,
-          addressesProviderRegistryOwner: deployer,
-          addressesProviderOwner: deployer,
-          wrappedTokenGatewayOwner: deployer,
-          emissionManagerOwner: deployer,
-          aclAdmin: deployer,
-          fundsAdmin: deployer,
-          poolAdmin: deployer,
-          emergencyAdmin: deployer,
-          rewardsAdmin: deployer,
-          rewardsHolder: deployer,
-          marketId: '0',
-          flashloanPremiumsTotal: 0n,
-          flashloanPremiumsProtocol: 0n,
-          providerId: 36n,
-          chainlinkAggProxy: chainlinkEthFeeder,
-          chainlinkETHUSDAggProxy: chainlinkEthFeeder,
+    action = new DeployAaveV3Action({
+      params: {
+        treasuryOwner: deployer,
+        addressesProviderRegistryOwner: deployer,
+        addressesProviderOwner: deployer,
+        wrappedTokenGatewayOwner: deployer,
+        emissionManagerOwner: deployer,
+        aclAdmin: deployer,
+        fundsAdmin: deployer,
+        poolAdmin: deployer,
+        emergencyAdmin: deployer,
+        rewardsAdmin: deployer,
+        rewardsHolder: deployer,
+        marketId: '0',
+        flashloanPremiumsTotal: 0n,
+        flashloanPremiumsProtocol: 0n,
+        providerId: 36n,
+        chainlinkAggProxy: chainlinkEthFeeder,
+        chainlinkETHUSDAggProxy: chainlinkEthFeeder,
 
-          assets: [],
-          sources: [],
-          fallbackOracle: zeroAddress,
-          baseCurrency: zeroAddress,
-          baseCurrencyUnit: 100_000_000n,
-          wrappedNativeToken: weth,
+        assets: [],
+        sources: [],
+        fallbackOracle: zeroAddress,
+        baseCurrency: zeroAddress,
+        baseCurrencyUnit: 100_000_000n,
+        wrappedNativeToken: weth,
 
-          defaultReserveInterestRateStrategyConfigs: [
-            {
-              name: 'rateStrategyVolatileOne',
-              params: {
-                optimalUsageRatio: parseUnits('0.45', 27),
-                baseVariableBorrowRate: 0n,
-                variableRateSlope1: parseUnits('0.07', 27),
-                variableRateSlope2: parseUnits('3', 27),
-                stableRateSlope1: parseUnits('0.07', 27),
-                stableRateSlope2: parseUnits('3', 27),
-                baseStableRateOffset: parseUnits('0.02', 27),
-                stableRateExcessOffset: parseUnits('0.05', 27),
-                optimalStableToTotalDebtRatio: parseUnits('0.2', 27),
-              },
-            },
-          ],
-        },
-        signer: {
-          deployer: client,
-        },
-      })
-      const curRegistry = await action.run({}, undefined, undefined)
-
-      const registryKeys: (keyof AaveV3Registry)[] = ['poolAddressProviderRegistry', 'supplyLogic']
-
-      expect(registryKeys.every((key) => !!curRegistry[key])).toBe(true)
-
-      expect(curRegistry.poolProxy).not.toBeUndefined()
-      expect(curRegistry.reservesSetupHelper).not.toBeUndefined()
-      expect(curRegistry.poolConfiguratorProxy).not.toBeUndefined()
-      expect(curRegistry.aclManager).not.toBeUndefined()
-
-      expect(curRegistry.poolProxy).not.toBe(zeroAddress)
-      expect(curRegistry.reservesSetupHelper).not.toBe(zeroAddress)
-      expect(curRegistry.poolConfiguratorProxy).not.toBe(zeroAddress)
-      expect(curRegistry.aclManager).not.toBe(zeroAddress)
-
-      const action2 = new SupportNewReserveAction({
-        signer: {
-          deployer: client,
-          poolAdmin: client,
-          aclAdmin: client,
-        },
-        params: {
-          aclManager: curRegistry.aclManager!,
-          pool: curRegistry.poolProxy!,
-          reservesSetupHelper: curRegistry.reservesSetupHelper!,
-          poolConfigurator: curRegistry.poolConfiguratorProxy!,
-          oracle: curRegistry.aaveOracle!,
-          setupReservesParams: [
-            {
-              aTokenImpl: curRegistry.aTokenImpl!,
-              stableDebtTokenImpl: curRegistry.stableDebtTokenImpl!,
-              variableDebtTokenImpl: curRegistry.variableDebtTokenImpl!,
-              underlyingAssetDecimals: 18,
-              interestRateStrategyAddress: curRegistry.reserveInterestRateStrategies!['rateStrategyVolatileOne']!,
-              underlyingAsset: weth,
-              treasury: curRegistry.aaveEcosystemReserveV2Proxy!,
-              incentivesController: zeroAddress,
-              aTokenName: 'eieize aToken',
-              aTokenSymbol: 'EIEI',
-              variableDebtTokenName: 'eieize aToken',
-              variableDebtTokenSymbol: 'EIEI V',
-              stableDebtTokenName: 'eieize aToken',
-              stableDebtTokenSymbol: 'EIEI V',
-              params: '0x10',
-              asset: weth,
-              baseLTV: 7000n,
-              liquidationThreshold: 7100n,
-              liquidationBonus: 11500n,
-              reserveFactor: 2000n,
-              borrowCap: 0n,
-              supplyCap: 0n,
-              stableBorrowingEnabled: false,
-              borrowingEnabled: true,
-              flashLoanEnabled: true,
-              amount: 100n,
-              onBehalfOf: client.walletClient.account.address,
-              referalCode: 0,
-              source: chainlinkEthFeeder,
-            },
-          ],
-        },
-      })
-      const curRegistry2 = await action2.run(curRegistry, undefined, undefined)
-
-      // test supply
-      const poolArtifact = await readArtifact('Pool')
-
-      // mint and approved WETH
-      await aliceClient.sendTransactions([
-        {
-          name: 'Mint WETH',
-          txData: {
-            data: encodeFunctionData({ abi: weth9.abi, functionName: 'deposit', args: [] }),
-            to: weth,
-            value: BigInt(10 ** 18),
-          },
-        },
-        {
-          name: 'Approve WETH',
-          txData: {
-            data: encodeFunctionData({ abi: weth9.abi, functionName: 'approve', args: [curRegistry2.poolProxy!, maxUint256] }),
-            to: weth,
-          },
-        },
-      ])
-
-      // supply to the pool
-      await expect(
-        aliceClient.sendTransactions([
+        defaultReserveInterestRateStrategyConfigs: [
           {
-            name: 'Supply WETH',
-            txData: {
-              data: encodeFunctionData({
-                abi: poolArtifact.abi,
-                functionName: 'supply',
-                args: [weth, BigInt(10 ** 17), aliceClient.walletClient.account.address, 0],
-              }),
-              to: curRegistry2.poolProxy,
+            name: 'rateStrategyVolatileOne',
+            params: {
+              optimalUsageRatio: parseUnits('0.45', 27),
+              baseVariableBorrowRate: 0n,
+              variableRateSlope1: parseUnits('0.07', 27),
+              variableRateSlope2: parseUnits('3', 27),
+              stableRateSlope1: parseUnits('0.07', 27),
+              stableRateSlope2: parseUnits('3', 27),
+              baseStableRateOffset: parseUnits('0.02', 27),
+              stableRateExcessOffset: parseUnits('0.05', 27),
+              optimalStableToTotalDebtRatio: parseUnits('0.2', 27),
             },
           },
-        ]),
-      ).resolves.not.toThrowError()
+        ],
+      },
+      signer: {
+        deployer: client,
+      },
+    })
+    const curRegistry = await action.run({}, undefined, undefined)
 
-      // borrow from the pool
-      await expect(
-        aliceClient.sendTransactions([
+    const registryKeys: (keyof AaveV3Registry)[] = ['poolAddressProviderRegistry', 'supplyLogic']
+
+    expect(registryKeys.every((key) => !!curRegistry[key])).toBe(true)
+
+    expect(curRegistry.poolProxy).not.toBeUndefined()
+    expect(curRegistry.reservesSetupHelper).not.toBeUndefined()
+    expect(curRegistry.poolConfiguratorProxy).not.toBeUndefined()
+    expect(curRegistry.aclManager).not.toBeUndefined()
+
+    expect(curRegistry.poolProxy).not.toBe(zeroAddress)
+    expect(curRegistry.reservesSetupHelper).not.toBe(zeroAddress)
+    expect(curRegistry.poolConfiguratorProxy).not.toBe(zeroAddress)
+    expect(curRegistry.aclManager).not.toBe(zeroAddress)
+
+    const action2 = new SupportNewReserveAction({
+      signer: {
+        deployer: client,
+        poolAdmin: client,
+        aclAdmin: client,
+      },
+      params: {
+        aclManager: curRegistry.aclManager!,
+        pool: curRegistry.poolProxy!,
+        reservesSetupHelper: curRegistry.reservesSetupHelper!,
+        poolConfigurator: curRegistry.poolConfiguratorProxy!,
+        oracle: curRegistry.aaveOracle!,
+        setupReservesParams: [
           {
-            name: 'Borrow WETH',
-            txData: {
-              data: encodeFunctionData({
-                abi: poolArtifact.abi,
-                functionName: 'borrow',
-                args: [weth, BigInt(10 ** 16), BigInt(2), 0, aliceClient.walletClient.account.address],
-              }),
-              to: curRegistry2.poolProxy,
-            },
+            aTokenImpl: curRegistry.aTokenImpl!,
+            stableDebtTokenImpl: curRegistry.stableDebtTokenImpl!,
+            variableDebtTokenImpl: curRegistry.variableDebtTokenImpl!,
+            underlyingAssetDecimals: 18,
+            interestRateStrategyAddress: curRegistry.reserveInterestRateStrategies!['rateStrategyVolatileOne']!,
+            underlyingAsset: weth,
+            treasury: curRegistry.aaveEcosystemReserveV2Proxy!,
+            incentivesController: zeroAddress,
+            aTokenName: 'eieize aToken',
+            aTokenSymbol: 'EIEI',
+            variableDebtTokenName: 'eieize aToken',
+            variableDebtTokenSymbol: 'EIEI V',
+            stableDebtTokenName: 'eieize aToken',
+            stableDebtTokenSymbol: 'EIEI V',
+            params: '0x10',
+            asset: weth,
+            baseLTV: 7000n,
+            liquidationThreshold: 7100n,
+            liquidationBonus: 11500n,
+            reserveFactor: 2000n,
+            borrowCap: 0n,
+            supplyCap: 0n,
+            stableBorrowingEnabled: false,
+            borrowingEnabled: true,
+            flashLoanEnabled: true,
+            amount: 100n,
+            onBehalfOf: client.walletClient.account.address,
+            referalCode: 0,
+            source: chainlinkEthFeeder,
           },
-        ]),
-      ).resolves.not.toThrowError()
+        ],
+      },
+    })
+    const curRegistry2 = await action2.run(curRegistry, undefined, undefined)
 
-      // repay to the pool
-      await expect(
-        aliceClient.sendTransactions([
-          {
-            name: 'Repay WETH',
-            txData: {
-              data: encodeFunctionData({
-                abi: poolArtifact.abi,
-                functionName: 'repay',
-                args: [weth, BigInt(10 ** 16), BigInt(2), aliceClient.walletClient.account.address],
-              }),
-              to: curRegistry2.poolProxy,
-            },
-          },
-        ]),
-      ).resolves.not.toThrowError()
+    // test supply
+    const poolArtifact = await readArtifact('Pool')
 
-      const aliceBalBf = await aliceClient.publicClient.readContract({
-        address: weth,
-        abi: weth9.abi,
-        functionName: 'balanceOf',
-        args: [aliceClient.walletClient.account.address],
-      })
+    // mint and approved WETH
+    await aliceClient.sendTransactions([
+      {
+        name: 'Mint WETH',
+        txData: {
+          data: encodeFunctionData({ abi: weth9.abi, functionName: 'deposit', args: [] }),
+          to: weth,
+          value: BigInt(10 ** 18),
+        },
+      },
+      {
+        name: 'Approve WETH',
+        txData: {
+          data: encodeFunctionData({ abi: weth9.abi, functionName: 'approve', args: [curRegistry2.poolProxy!, maxUint256] }),
+          to: weth,
+        },
+      },
+    ])
 
-      await aliceClient.sendTransactions([
+    // supply to the pool
+    await expect(
+      aliceClient.sendTransactions([
         {
-          name: 'Withdraw WETH',
+          name: 'Supply WETH',
           txData: {
             data: encodeFunctionData({
               abi: poolArtifact.abi,
-              functionName: 'withdraw',
-              args: [weth, BigInt(2 ** 16), aliceClient.walletClient.account.address],
+              functionName: 'supply',
+              args: [weth, BigInt(10 ** 17), aliceClient.walletClient.account.address, 0],
             }),
             to: curRegistry2.poolProxy,
           },
         },
-      ])
+      ]),
+    ).resolves.not.toThrowError()
 
-      const aliceBalAft = await aliceClient.publicClient.readContract({
-        address: weth,
-        abi: weth9.abi,
-        functionName: 'balanceOf',
-        args: [aliceClient.walletClient.account.address],
-      })
+    // borrow from the pool
+    await expect(
+      aliceClient.sendTransactions([
+        {
+          name: 'Borrow WETH',
+          txData: {
+            data: encodeFunctionData({
+              abi: poolArtifact.abi,
+              functionName: 'borrow',
+              args: [weth, BigInt(10 ** 16), BigInt(2), 0, aliceClient.walletClient.account.address],
+            }),
+            to: curRegistry2.poolProxy,
+          },
+        },
+      ]),
+    ).resolves.not.toThrowError()
 
-      await expect(aliceBalBf + BigInt(2 ** 16)).toBe(aliceBalAft)
-    },
-    { retry: 5 },
-  )
+    // repay to the pool
+    await expect(
+      aliceClient.sendTransactions([
+        {
+          name: 'Repay WETH',
+          txData: {
+            data: encodeFunctionData({
+              abi: poolArtifact.abi,
+              functionName: 'repay',
+              args: [weth, BigInt(10 ** 16), BigInt(2), aliceClient.walletClient.account.address],
+            }),
+            to: curRegistry2.poolProxy,
+          },
+        },
+      ]),
+    ).resolves.not.toThrowError()
+
+    const aliceBalBf = await aliceClient.publicClient.readContract({
+      address: weth,
+      abi: weth9.abi,
+      functionName: 'balanceOf',
+      args: [aliceClient.walletClient.account.address],
+    })
+
+    await aliceClient.sendTransactions([
+      {
+        name: 'Withdraw WETH',
+        txData: {
+          data: encodeFunctionData({
+            abi: poolArtifact.abi,
+            functionName: 'withdraw',
+            args: [weth, BigInt(2 ** 16), aliceClient.walletClient.account.address],
+          }),
+          to: curRegistry2.poolProxy,
+        },
+      },
+    ])
+
+    const aliceBalAft = await aliceClient.publicClient.readContract({
+      address: weth,
+      abi: weth9.abi,
+      functionName: 'balanceOf',
+      args: [aliceClient.walletClient.account.address],
+    })
+
+    await expect(aliceBalBf + BigInt(2 ** 16)).toBe(aliceBalAft)
+  })
 })
