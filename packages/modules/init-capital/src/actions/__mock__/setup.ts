20d72062cfabe918692814ce9bba439baa22e694
mewpawee <pawee@alphainnovationslab.io>
2024-11-11 18:13:58 +0700

    chore(init-capital): remove unused code

--- packages/modules/init-capital/src/actions/__mock__/setup.ts
+++ packages/modules/init-capital/src/actions/__mock__/setup.ts
@@ -33,44 +33,6 @@ export const setupInitCapital = async (): Promise<InitCapitalRegistry> => {
       guardian: deployer,
       feeAdmin: accessControlManagerOwner,
       treasury: accessControlManagerOwner,
-      doubleSlopeIRMConfigs: [
-        {
-          name: 'StablecoinIRM',
-          params: {
-            baseBorrowRateE18: 100000000000000000n,
-            jumpUtilizationRateE18: 800000000000000000n,
-            borrowRateMultiplierE18: 10000000000000000n,
-            jumpRateMultiplierE18: 10000000000000000n,
-          },
-        },
-        {
-          name: 'MajorcoinIRM',
-          params: {
-            baseBorrowRateE18: 100000000000000000n,
-            jumpUtilizationRateE18: 600000000000000000n,
-            borrowRateMultiplierE18: 10000000000000000n,
-            jumpRateMultiplierE18: 10000000000000000n,
-          },
-        },
-        {
-          name: 'MemecoinIRM',
-          params: {
-            baseBorrowRateE18: 300000000000000000n,
-            jumpUtilizationRateE18: 500000000000000000n,
-            borrowRateMultiplierE18: 20000000000000000n,
-            jumpRateMultiplierE18: 50000000000000000n,
-          },
-        },
-        {
-          name: 'LrtcoinIRM',
-          params: {
-            baseBorrowRateE18: 100000000000000000n,
-            jumpUtilizationRateE18: 500000000000000000n,
-            borrowRateMultiplierE18: 10000000000000000n,
-            jumpRateMultiplierE18: 10000000000000000n,
-          },
-        },
-      ],
     },
     signer: {
       deployer: client,
