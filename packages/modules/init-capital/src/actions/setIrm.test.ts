e3b130be714c17c018ac21673bd37cd98a902166
kittikornl <pkorn003@gmail.com>
2024-11-12 02:32:31 +0700

    test(init-capital): add test for set new irm

--- packages/modules/init-capital/src/actions/setIrm.test.ts
+++ packages/modules/init-capital/src/actions/setIrm.test.ts
@@ -1,12 +1,15 @@
-import { describe, expect, test } from 'vitest'
+import { beforeAll, describe, expect, test } from 'vitest'
 
 import { SubAction } from '@infinit-xyz/core'
 
 import { ARBITRUM_TEST_ADDRESSES } from '@actions/__mock__/address'
+import { setupInitCapital } from '@actions/__mock__/setup'
 
 import { SetIrmAction, SetIrmActionData } from './setIrm'
 import { SetIrmSubAction } from './subactions/setIrm'
+import { InitCapitalRegistry } from '@/src/type'
 import { TestChain, TestInfinitWallet } from '@infinit-xyz/test'
+import { readArtifact } from '@utils/artifact'
 
 class SetIrmActionTest extends SetIrmAction {
   public override getSubActions(): SubAction[] {
@@ -17,6 +20,33 @@ class SetIrmActionTest extends SetIrmAction {
 const tester = ARBITRUM_TEST_ADDRESSES.tester
 describe('SetIrm', async () => {
   const client = new TestInfinitWallet(TestChain.arbitrum, tester)
+  let registry: InitCapitalRegistry
+
+  beforeAll(async () => {
+    registry = await setupInitCapital()
+  })
+
+  test('test set new irm', async () => {
+    const data: SetIrmActionData = {
+      signer: { guardian: client },
+      params: {
+        pool: registry.lendingPools!['INIT Ether'].lendingPool,
+        irm: '0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf',
+      },
+    }
+    const action = new SetIrmAction(data)
+    const curRegistry = await action.run(registry)
+    const lendingPoolArtifact = await readArtifact('LendingPool')
+    const irm = await client.publicClient.readContract({
+      address: registry.lendingPools!['INIT Ether'].lendingPool,
+      abi: lendingPoolArtifact.abi,
+      functionName: 'irm',
+      args: [],
+    })
+    expect(irm).toBe('0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf')
+    expect(curRegistry.lendingPools!['INIT Ether'].irm).toBe('0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf')
+  })
+
   test('test correct name', async () => {
     expect(SetIrmAction.name).toStrictEqual('SetIrmAction')
   })
