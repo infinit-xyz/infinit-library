7429ee81dac71ba044b6460c515bbfb65f7cc75a
mewpawee <pawee@alphainnovationslab.io>
2024-11-12 12:35:54 +0700

    feat(init-capital): add check non-negative for irm params

--- packages/modules/init-capital/src/actions/changeIrm.ts
+++ packages/modules/init-capital/src/actions/changeIrm.ts
@@ -20,12 +20,13 @@ export const ChangeIrmActionParamsSchema = z.object({
       name: z.string().describe(`Name of the reserve interest rate model that will be displayed in the registry`),
       params: z
         .object({
-          baseBorrowRateE18: z.bigint().describe(`Base borrow rate in E18 (e.g., 10% = 0.1 * 1e18)`),
+          baseBorrowRateE18: z.bigint().nonnegative().describe(`Base borrow rate in E18 (e.g., 10% = 0.1 * 1e18)`),
           jumpUtilizationRateE18: z
             .bigint()
+            .nonnegative()
             .describe(`Utilization rate in E18 where the jump multiplier is applied (e.g., 80% = 0.8 * 1e18)`),
-          borrowRateMultiplierE18: z.bigint().describe(`Borrow rate multiplier in E18 (e.g., 1% = 0.01 * 1e18)`),
-          jumpRateMultiplierE18: z.bigint().describe(`Jump multiplier rate in E18 (e.g., 1% = 0.01 * 1e18)`),
+          borrowRateMultiplierE18: z.bigint().nonnegative().describe(`Borrow rate multiplier in E18 (e.g., 1% = 0.01 * 1e18)`),
+          jumpRateMultiplierE18: z.bigint().nonnegative().describe(`Jump multiplier rate in E18 (e.g., 1% = 0.01 * 1e18)`),
         })
         .describe(
           `Parameters for the interest rate model => real borrow rate = baseRate + borrowRate * min(currentUtil, jumpUtil) + jumpRate * max(0, uti - jumpUtil)`,
