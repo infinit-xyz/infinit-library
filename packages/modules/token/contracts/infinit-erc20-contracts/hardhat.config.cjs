91d21b31a23e4b0b2a7bcdcdbf4b0149c38e0f21
tapakornl <tapakorn.loet@gmail.com>
2024-12-02 16:37:45 +0700

    fix: bug

--- packages/modules/token/contracts/infinit-erc20-contracts/hardhat.config.cjs
+++ packages/modules/token/contracts/infinit-erc20-contracts/hardhat.config.cjs
@@ -2,7 +2,7 @@ const { name } = require('../../package.json')
 // NOTE: need to import like this for now to be compatible with hardhat commonjs
 const { baseHardhatUserConfig } = require('@infinit-xyz/core/internal/hardhat-base-config')
 
-const remappings = [`@openzeppelin/contracts=@openzeppelin/contracts-5.0.0`]
+const remappings = [`@openzeppelin/contracts/=@openzeppelin/contracts-5.0.0/`]
 
 const config = {
   ...baseHardhatUserConfig(__dirname, name, remappings),
