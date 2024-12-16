de7236f307e1955c03adedda361b7ef8363d379e
kittikornl <pkorn003@gmail.com>
2024-12-16 12:07:35 +0700

    feat(pendle): add new contracts

--- packages/modules/pendle/contracts/openzeppelin/toCompile.sol
+++ packages/modules/pendle/contracts/openzeppelin/toCompile.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.8.19;
 
 // note: this file is to be used for compiling the contracts in the node_modules
-import "@openzeppelin-contracts/proxy/transparent/ProxyAdmin.sol";
-import "@openzeppelin-contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
