5005f7283d64d708dcd437b2cae66b23d53b60a9
mewpawee <pawee@alphainnovationslab.io>
2024-12-17 10:13:12 +0700

    add deploy and test for subaction

--- packages/modules/pendle/src/type.ts
+++ packages/modules/pendle/src/type.ts
@@ -1,1 +1,11 @@
-export type PendleRegistry = {}
+import { Address } from 'viem'
+
+export type PendleRegistry = {
+  multicall?: Address
+  pendleMulticallV2?: Address
+  simulateHelper?: Address
+  supplyCapReader?: Address
+  pendleGovernanceProxy?: Address
+  pendleGovernanceProxyImpl?: Address
+  pendleBoringOneracle?: Address
+}
