77ebc16eb74c477fe851f017c6483b3f366e70dc
Pawee Tantivasdakarn <pawee@alphainnovationslab.io>
2024-12-30 17:18:15 +0700

    fix tsup config name

--- packages/modules/pendle/tsup.config.ts
+++ packages/modules/pendle/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  name: 'init-capital',
+  name: 'pendle',
   entry: ['src/index.ts', 'src/actions/index.ts', 'src/utils/index.ts'],
   format: ['esm', 'cjs'], // Build for ESmodules and Commonjs
   dts: true, // Generate declaration file (.d.ts)
