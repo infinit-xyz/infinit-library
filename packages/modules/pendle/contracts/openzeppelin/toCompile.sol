7c6a86135efcd4dbbc2337caeb622f286af52789
kittikornl <pkorn003@gmail.com>
2025-01-07 16:30:15 +0700

    fix(pendle): add toCompile.sol

---
+++ packages/modules/pendle/contracts/openzeppelin/toCompile.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.23;
+
+// note: this file is to be used for compiling the contracts in the node_modules
+import "@openzeppelin-contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin-contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
