6af06c578349065c67c07f37cb6740e140e6ea45
Pawee Tantivasdakarn <pawee@alphainnovationslab.io>
2025-01-13 17:02:26 +0800

    fix(core): fix projectRoot in the sourceName (#197)

--- packages/core/src/utils/contract-verifier/helper/getDependencyGraph.ts
+++ packages/core/src/utils/contract-verifier/helper/getDependencyGraph.ts
@@ -1,4 +1,3 @@
-import path from 'path'
 
 import { DirectoryNotFoundError } from '@/errors'
 import { DependencyGraph } from 'hardhat/internal/solidity/dependencyGraph.js'
@@ -8,7 +7,7 @@ import * as fs from 'node:fs/promises'
 
 export const getDependencyGraph = async (sourceName: string, projectRoot: string): Promise<DependencyGraph> => {
   const parser = new Parser()
-  const remappings = await getRemappings(projectRoot, sourceName)
+  const remappings = await getRemappings(sourceName)
 
   const resolver = new Resolver(
     projectRoot,
@@ -43,8 +42,7 @@ const transformImportName = async ({ importName }: { importName: string }): Prom
 
 // NOTE: quick fix here
 // TODO: use remappings from remappings file in contract project along with hardhatconfig
-const getRemappings = async (projectRoot: string, sourceName: string): Promise<Record<string, string>> => {
-  const sourceNameWithRoot = path.resolve(path.basename(projectRoot), sourceName)
+const getRemappings = async (sourceName: string): Promise<Record<string, string>> => {
   let remappings: Record<string, string> = {}
 
   // fee-vault's remappings
@@ -53,7 +51,7 @@ const getRemappings = async (projectRoot: string, sourceName: string): Promise<R
   }
 
   // init-capital: init-capital's remappings
-  else if (sourceNameWithRoot.startsWith('init-capital/init-capital')) {
+  else if (sourceName.startsWith('init-capital')) {
     remappings = {
       '@openzeppelin-contracts/': '@openzeppelin/contracts-4.9.3/',
       '@openzeppelin-contracts-upgradeable/': '@openzeppelin/contracts-upgradeable-4.9.3/',
@@ -61,7 +59,9 @@ const getRemappings = async (projectRoot: string, sourceName: string): Promise<R
   }
 
   // init-capital: openzeppelin's remappings
-  else if (sourceNameWithRoot.startsWith('init-capital/openzeppelin')) {
+  // TODO: need to handle if there are openzeppelin under the
+  // different modules that use the different version of openzeppelin mappings
+  else if (sourceName.startsWith('openzeppelin')) {
     remappings = {
       '@openzeppelin-contracts/': '@openzeppelin/contracts-4.9.3/',
       '@openzeppelin-contracts-upgradeable/': '@openzeppelin/contracts-upgradeable-4.9.3/',
@@ -69,24 +69,21 @@ const getRemappings = async (projectRoot: string, sourceName: string): Promise<R
   }
 
   // token: infinit-erc20-contracts's remappings
-  else if (sourceNameWithRoot.startsWith('token/infinit-erc20-contracts')) {
+  else if (sourceName.startsWith('infinit-erc20-contracts')) {
     remappings = {
       '@openzeppelin/contracts/': '@openzeppelin/contracts-5.0.0/',
     }
   }
 
   // uniswap-v3: openzeppelins@3.4.1 and swap router contract's remappings
-  else if (
-    sourceNameWithRoot.startsWith('uniswap-v3/openzeppelin@3.4.1') ||
-    sourceNameWithRoot.startsWith('uniswap-v3/swap-router-contracts')
-  ) {
+  else if (sourceName.startsWith('openzeppelin@3.4.1') || sourceName.startsWith('swap-router-contracts')) {
     remappings = {
       '@openzeppelin/contracts': '@openzeppelin/contracts-3.4.1-solc-0.7-2',
     }
   }
 
   // uniswap-v3: universal router's remappings
-  else if (sourceNameWithRoot.startsWith('uniswap-v3/universal-router')) {
+  else if (sourceName.startsWith('universal-router')) {
     remappings = {
       '@openzeppelin/contracts': '@openzeppelin/contracts-4.7.0',
       'permit2/src': 'permit2/contracts',
@@ -94,14 +91,14 @@ const getRemappings = async (projectRoot: string, sourceName: string): Promise<R
   }
 
   // uniswap-v3: v3-periphery's remappings
-  else if (sourceNameWithRoot.startsWith('uniswap-v3/v3-periphery')) {
+  else if (sourceName.startsWith('v3-periphery')) {
     remappings = {
       '@openzeppelin/contracts': '@openzeppelin/contracts-3.4.2-solc-0.7',
     }
   }
 
   // uniswap-v3: v3-staker's remappings
-  else if (sourceNameWithRoot.startsWith('uniswap-v3/v3-staker')) {
+  else if (sourceName.startsWith('v3-staker')) {
     remappings = {
       '@openzeppelin/contracts': 'openzeppelin/contracts-3.4.1-solc-0.7-2`',
     }
