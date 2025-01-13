8b50a25aa671ea23e9722103b498970f7f7b8eee
mewpawee <pawee@alphainnovationslab.io>
2025-01-13 11:13:29 +0700

    update remappings

--- packages/core/src/utils/contract-verifier/helper/getDependencyGraph.ts
+++ packages/core/src/utils/contract-verifier/helper/getDependencyGraph.ts
@@ -8,20 +8,59 @@ export const getDependencyGraph = async (sourceName: string, projectRoot: string
   const parser = new Parser()
 
   // NOTE: quick fix here
-  // TODO: Need to recheck why fee-vault and init-capital not remapped
+  // TODO: use remappings from remappings file in contract project along with hardhatconfig 
   let remappings: Record<string, string> = {}
 
   // fee-vault's remappings
   if (sourceName.startsWith('fee-vault')) {
     remappings = { '@openzeppelin-contracts/': '@openzeppelin/contracts-5.0.2/' }
   }
-  // init-capital's remappings
+  // init-capital: init-capital's remappings
   else if (sourceName.startsWith('init-capital')) {
     remappings = {
       '@openzeppelin-contracts/': '@openzeppelin/contracts-4.9.3/',
       '@openzeppelin-contracts-upgradeable/': '@openzeppelin/contracts-upgradeable-4.9.3/',
     }
   }
+  // init-capital: openzeppelin's remappings
+  else if (sourceName.startsWith('openzeppelin')) {
+    remappings = {
+      '@openzeppelin-contracts/': '@openzeppelin/contracts-4.9.3/',
+      '@openzeppelin-contracts-upgradeable/': '@openzeppelin/contracts-upgradeable-4.9.3/',
+    }
+  }
+  // token: infinit-erc20-contracts's remappings
+  else if (sourceName.startsWith('infinit-erc20-contracts')) {
+    remappings = {
+      '@openzeppelin/contracts/': '@openzeppelin/contracts-5.0.0/'
+    }
+  }
+  // uniswap-v3: openzeppelins@3.4.1 and swap router contract's remappings
+  else if (sourceName.startsWith('openzeppelin@3.4.1') || sourceName.startsWith('swap-router-contracts')) {
+    remappings = {
+      '@openzeppelin/contracts':'@openzeppelin/contracts-3.4.1-solc-0.7-2'
+    }
+  }
+  // uniswap-v3: universal router's remappings
+  else if (sourceName.startsWith('openzeppelin@3.4.1')) {
+    remappings = {
+      '@openzeppelin/contracts':'@openzeppelin/contracts-4.7.0',
+      'permit2/src':'permit2/contracts'
+    }
+  }
+  // uniswap-v3: v3-periphery's remappings
+  else if (sourceName.startsWith('v3-periphery')) {
+    remappings = {
+      '@openzeppelin/contracts':'@openzeppelin/contracts-3.4.2-solc-0.7'
+    }
+  }
+  // uniswap-v3: v3-staker's remappings
+  else if (sourceName.startsWith('v3-staker')) {
+    remappings = {
+      '@openzeppelin/contracts':'openzeppelin/contracts-3.4.1-solc-0.7-2`'
+    }
+  }
+  
 
   const resolver = new Resolver(
     projectRoot,
